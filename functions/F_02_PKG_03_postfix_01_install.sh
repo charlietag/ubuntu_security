6812dff54426d2851d829bbd491261534e015258
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-26 15:53:05 +0800

    postfix related packages from CentOS to Ubuntu based

--- functions/F_02_PKG_03_postfix_01_install.sh
+++ functions/F_02_PKG_03_postfix_01_install.sh
@@ -12,6 +12,16 @@
 #--------------------------------------
 # Start - install postfix
 #--------------------------------------
-dnf install -y postfix
-dnf install -y cyrus-sasl-md5 cyrus-sasl-plain cyrus-sasl
+apt install -y postfix
+
+# -----------
+# AUTH (cyrus)
+# -----------
+# --- might need for corp ---
+# libsasl2-modules-ldap/jammy-updates 2.1.27+dfsg2-3ubuntu1.1 amd64
+#   Cyrus SASL - pluggable authentication modules (LDAP)
+# --- might need for corp ---
+
+# libsasl2-2 shoud be installed after postfix installation
+apt install -y libsasl2-2 libsasl2-dev
 systemctl stop postfix
